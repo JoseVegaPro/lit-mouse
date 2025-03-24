0d6d9e2f0b3cda4374d1de99791d68e8313ad8cc
Jose Vega <j7.x@icloud.com>
2025-03-24 18:55:09 +0900

    Let's go

---
+++ Lit Mouse/Lit Mouse/Lit_MouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lit_MouseApp.swift
+//  Lit Mouse
+//
+//  Created by Jose Vega on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Lit_MouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
